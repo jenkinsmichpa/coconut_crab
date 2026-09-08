4b257f0a67644bfd575fa9b019af218586fe2486
Michael Jenkins <jenkinsmichpa@gmail.com>
2026-09-08 02:43:00 -0400

    add nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "coconut_crab";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
+      mkDevShell = pkgs: system: pkgs.mkShell {
+        packages = with pkgs; [ zig llvmPackages.libclang ];
+        env = pkgs.lib.optionalAttrs (system == "aarch64-darwin") {
+          BINDGEN_EXTRA_CLANG_ARGS_aarch64_apple_darwin = "--target=aarch64-apple-darwin";
+        };
+        shellHook = ''
+          export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+        '';
+      };
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system: {
+        default = mkDevShell nixpkgs.legacyPackages.${system} system;
+      });
+    };
+}
